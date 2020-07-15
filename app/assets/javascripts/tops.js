1a93f17a2c7f7eaa2233cbe83429cf48c41401b3
yuritanu <fool0628@icloud.com>
2020-07-15 17:48:37 +0900

    最初

---
+++ app/assets/javascripts/tops.js
@@ -0,0 +1,7 @@
+$(function(){
+
+  $(document).on('keypress', function(e){
+    alert('key:' + e.key);
+  });
+    
+});
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  root to:'tops#index'
+end
